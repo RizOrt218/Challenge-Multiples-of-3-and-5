7199cf233930a52e7dfbeb504814ca51a6ede55d
RizOrt218 <rizhiafalen@gmail.com>
2015-12-26 16:38:51 -1000

    first commit

--- test/multiples_spec.js
+++ test/multiples_spec.js
@@ -1,11 +1,11 @@
 var multiples = require( './../multiples.js' ); // import the file we are testing
-var chai = require( 'chai' );
+var chai = require( 'chai' ); //chai - language for test runner
 var should = chai.should();
 
 describe( 'Sum of a Multiple Function', function() {
 
   it( 'should return the value 23 if given 10 as an argument', function() {
-    var sum = multiples.sumOfAMultiple( 10 );
+    var sum = multiples.sumOfAMultiple( 10 ); //func calld 'sumOfAMultiple'
     sum.should.equal( 23 );
   });
 
@@ -16,20 +16,20 @@ describe( 'Sum of a Multiple Function', function() {
 
   // when your tests pass, uncomment the next two and
   // change the expected value to your calculated result
-  /*
+
   it( 'should return \'x\' for n=1000 (see multiples-spec.js)', function() {
     var sum = multiples.sumOfAMultiple(1000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(233168);
   });
 
   it( 'should return \'x\' for n=10000 (see multiples-spec.js)', function() {
     var sum = multiples.sumOfAMultiple(10000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(23331668);
   });
-  */
+
 
 });
